fc25ddb8eea627abf74a632fe4de2907c5562e49
Vladimir Zahariev <vlzahariev@gmail.com>
2023-12-21 21:23:53 +0200

    fixed UI tests

--- tests/ui/add-movie.test.js
+++ tests/ui/add-movie.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add movie page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-movie');
+    await page.goto('https:/vlzahariev.onrender.com/add-movie');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
