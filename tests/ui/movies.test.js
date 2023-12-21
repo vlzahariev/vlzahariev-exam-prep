7af3a608c7330b6ffd04b649d65496ee64786310
Vladimir Zahariev <vlzahariev@gmail.com>
2023-12-21 02:21:08 +0200

    commit changes

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('http://localhost:8080/collection');
+    await page.goto('http://localhost:8080/movies');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
